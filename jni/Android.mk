f07b77b761f6fffde9db3b6acbede422402dfc69
wtmmac <wtmmac@gmail.com>
2014-11-01 14:41:13 +0800

    up

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,8 +1,3 @@
-#project path
-#AVPLAYER_PATH := "/Users/ym/Documents/jobs/kudou/kudou2/pro/kudou2.0"
-#AVPLAYER_PATH := "/Users/tonywang/android_workspace/mem_player"
-
-
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
    LOCAL_CFLAGS += -DHAVE_NEON=1
 endif
